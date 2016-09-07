e64d864344a33787237d94b616be461777f1b11f
Chris McFee <christopher_mcfee@keybank.com>
2016-09-07 10:43:07 -0400

    add output lin

--- bin/metrics-pods.rb
+++ bin/metrics-pods.rb
@@ -28,11 +28,11 @@ require 'socket'
 
 class PodsMetrics < Sensu::Plugins::Kubernetes::CLI
   @options = Sensu::Plugins::Kubernetes::CLI.options.dup
-  option :scheme,
-       description: 'Metric naming scheme, text to prepend to metric',
-       short: '-s SCHEME',
-       long: '--scheme SCHEME',
-       default: "#{Socket.gethostname}.pods"
+#  option :scheme,
+#       description: 'Metric naming scheme, text to prepend to metric',
+#       short: '-s SCHEME',
+#       long: '--scheme SCHEME',
+#       default: "#{Socket.gethostname}.pods"
 
   def run
     pod_counts = []
@@ -56,6 +56,7 @@ class PodsMetrics < Sensu::Plugins::Kubernetes::CLI
         count[s.metadata.name] += 1
       end
     end
+    count.size.times { |k,v| output "#{config[:scheme]}.#{k}", v}
     puts "DEBUG #{count}"
     ok
   end
