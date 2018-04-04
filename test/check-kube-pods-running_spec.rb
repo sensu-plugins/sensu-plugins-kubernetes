8481379dad1a777750a2cbc7fc4b0f2c747f0cf2
Rene Martin <rene_martin@intuit.com>
2018-04-04 16:15:26 -0700

    Rubocop is happy again

--- test/check-kube-pods-running_spec.rb
+++ test/check-kube-pods-running_spec.rb
@@ -28,20 +28,20 @@ describe AllPodsAreRunning do
   describe 'is the pod ready?' do
     it 'returns false if there is no type Ready' do
       pod = instance_double('pod')
-      allow(pod).to receive_message_chain(:status, :conditions => [])
-      expect(check.is_it_ready? pod).to eq(false)
+      allow(pod).to receive_message_chain(:status, conditions: [])
+      expect(check.ready?(pod)).to eq(false)
     end
 
     it 'returns false if there is type Ready but the status is not true' do
       pod = instance_double('pod')
-      allow(pod).to receive_message_chain(:status, :conditions => [instance_double('status_ready', :type => 'Ready', :status => 'False')])
-      expect(check.is_it_ready? pod).to eq(false)
+      allow(pod).to receive_message_chain(:status, conditions: [instance_double('status_ready', type: 'Ready', status: 'False')])
+      expect(check.ready?(pod)).to eq(false)
     end
 
     it 'returns true if there is type Ready and status is true' do
       pod = instance_double('pod')
-      allow(pod).to receive_message_chain(:status, :conditions => [instance_double('status_ready', :type => 'Ready', :status => 'True')])
-      expect(check.is_it_ready? pod).to eq(true)
+      allow(pod).to receive_message_chain(:status, conditions: [instance_double('status_ready', type: 'Ready', status: 'True')])
+      expect(check.ready?(pod)).to eq(true)
     end
   end
 end
